29125c904294f20b68aef2970bcc6f8208796b91
Cl0udG0d <2585614464@qq.com>
2021-04-22 23:05:42 +0800

    add base code2

--- index.py
+++ index.py
@@ -1,17 +1,14 @@
+from flask import request,render_template
 from init import app
-from app.users import users
-from app.admin import admin
-from app.tourist import tourist
 
-from models import Role
 
-Role.init_role()
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return render_template('index.html')
 
-# 注册蓝图
-app.register_blueprint(admin, url_prefix='/admin')
-app.register_blueprint(users, url_prefix='/users')
-app.register_blueprint(tourist)
-
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
