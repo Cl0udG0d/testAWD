10772f08c59ca7076bc7c6f28a96523432f34c4f
Cl0udG0d <2585614464@qq.com>
2021-04-24 23:58:48 +0800

    edit unity_server

--- unity/server.py
+++ unity/server.py
@@ -1,43 +1,64 @@
-# import socket
-# # 建立一个服务端
-# server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# server.bind(('localhost',19999)) #绑定要监听的端口
-# server.listen(5) #开始监听 表示可以使用五个链接排队
-# while True:# conn就是客户端链接过来而在服务端为期生成的一个链接实例
-#     conn,addr = server.accept() #等待链接,多个链接的时候就会出现问题,其实返回了两个值
-#     print(conn,addr)
-#     while True:
-#         try:
-#             data = conn.recv(1024)  #接收数据
-#             print('recive:',data.decode()) #打印接收到的数据
-#             conn.send(data.upper()) #然后再发送数据
-#         except Exception as e:
-#             print('关闭了正在占线的链接！')
-#             print(e)
-#             break
-#     conn.close()
-
-import socket
 import json
-sendData = {
-  "Type":"INIT",
-  "Data":{
-          "Title":"aaa",
-          "Team":[{}],
-          "Time":1000,
-          "Round":10,
+import logging
+from websocket_server import WebsocketServer
+
+
+data={
+    "Type":"init",
+    "Data":{
+        "Title":"Test",
+        "Team":[
+            {
+                "Id":1,
+                "Name":"team1",
+                "Score":"100",
+                "Rank":"100",
+
+            },
+            {
+                "Id":2,
+                "Name":"team2",
+                "Score":"100",
+                "Rank":"100",
+
+            },
+            {
+                "Id":3,
+                "Name":"team3",
+                "Score":"100",
+                "Rank":"100",
+
+            }
+        ],
+        "Time":"100",
+        "Round":"10",
     }
 }
-BUF_SIZE = 1024
-host = 'localhost'
-port = 19999
+def new_client(client, server):
+    print("Client(%d) has joined." % client['id'])
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen(1)  # 接收的连接数
-client, address = server.accept()  # 因为设置了接收连接数为1，所以不需要放在循环中接收
-while True:  # 循环收发数据包，长连接
-    data = client.recv(BUF_SIZE)
-    print(data.decode())  # python3 要使用decode
-    # client.close() #连接不断开，长连接
-    client.send(bytes(repr(json.dumps(sendData)).encode('utf-8')))
+
+def client_left(client, server):
+    print("Client(%d) disconnected" % client['id'])
+
+
+def message_back(client, server, message):
+    # 这里的message参数就是客户端传进来的内容
+    print("Client(%d) said: %s" % (client['id'], message))
+    # 这里可以对message进行各种处理
+    # result = "服务器已经收到消息了..." + message
+    # 将处理后的数据再返回给客户端
+    server.send_message(client, bytes(repr(json.dumps(data)).encode('utf-8')))
+
+
+# 新建一个WebsocketServer对象，第一个参数是端口号，第二个参数是host
+# 如果host为空，则默认为本机IP
+server = WebsocketServer(19999, host='127.0.0.1')
+# 设置当有新客户端接入时的动作
+server.set_fn_new_client(new_client)
+# 设置当有客户端断开时的动作
+server.set_fn_client_left(client_left)
+# 设置当接收到某个客户端发送的消息后的操作
+server.set_fn_message_received(message_back)
+# 设置服务一直运行
+server.run_forever()
