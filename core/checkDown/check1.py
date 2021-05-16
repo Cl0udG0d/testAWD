eafd9b1be98407a299c92c2cd271a72d6096cf19
Cl0udG0d <2585614464@qq.com>
2021-05-17 00:32:47 +0800

    修改部分代码

--- core/checkDown/check1.py
+++ core/checkDown/check1.py
@@ -2,15 +2,16 @@ import requests
 
 def check(target_ip, target_port):
     """web_example"""
-    try:
-        # print(target_ip+":"+target_port)
-        res = requests.get('http://{}:{}'.format(target_ip,target_port))
-        if res.status_code == 200:
-            return True
-        return False
-    except Exception as e:
-        print(e)
-        pass
+    # try:
+    #     # print(target_ip+":"+target_port)
+    #     res = requests.get('http://{}:{}'.format(target_ip,target_port))
+    #     if res.status_code == 200:
+    #         return True
+    #     return False
+    # except Exception as e:
+    #     print(e)
+    #     pass
+    return True
 
 
 
