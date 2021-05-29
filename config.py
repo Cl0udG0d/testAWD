c25bbab5adafc215ee6564e4581a3618580f7b06
黄漂漂 <1608276316@qq.com>
2021-05-29 13:12:25 +0800

    edit html

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'FanDu_AWD'
 USERNAME = 'root'
-# PASSWORD = '123mysql'
-PASSWORD = 'root'
+PASSWORD = '123mysql'
+# PASSWORD = 'root'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT,
                                                                                DATABASE)
